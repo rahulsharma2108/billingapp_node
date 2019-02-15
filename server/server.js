d4bce9c51fec6b743a2429ee2ae7780c3a3b9f1e
Rahul Sharma <sharma-rahu2@hcl.com>
2019-02-15 15:56:31 +0000

    added rest

--- server/server.js
+++ server/server.js
@@ -1,29 +1,27 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/billing');
+var {mongoose}  = require('./db/mongoose');
+var {Todo} = require('./models/todos');
 
 // create Modal for Todo
 
-var Todo = mongoose.model('Todo',{
-	text:{
-		type:String
-	},
-	completed:{
-		type:Boolean
-	},
-	completedAt:{
-		type:Number
-	}
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+	var todo = new Todo({
+		text: req.body.text
+	});
+
+	todo.save().then((doc)=>{
+		res.send(doc);	
+	},(e)=>{
+		res.status(400).send(e);
+	});
 });
 
-
-var newTodo = new Todo({
-	text:'Cool dinner'
-});
-
-newTodo.save().then((doc)=>{
-	console.log('saved todo', doc);
-},(e)=>{
-	console.log('Unable to save todo')
+app.listen(3000,()=>{
+	console.log('started at 3000');
 });
