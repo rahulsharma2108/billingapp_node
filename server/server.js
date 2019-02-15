ad7104e9b43cd0661adf2384ab931d96ecb82131
Rahul Sharma <sharma-rahu2@hcl.com>
2019-02-15 16:29:04 +0000

    Added get calls

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose}  = require('./db/mongoose');
 var {Todo} = require('./models/todos');
@@ -22,6 +23,40 @@ app.post('/todos',(req,res)=>{
 	});
 });
 
+app.get('/todos',(req,res)=>{
+	Todo.find().then((todos)=>{
+		res.send({
+			todos
+		});
+	},(e)=>{
+		res.status(400).send(e);
+	});
+});
+
+app.get('/todos/:id',(req,res)=>{
+	 var id = req.params.id;
+
+	 // validate Id
+	 if(!ObjectID.isValid(id)){
+	 	return res.status(404).send();
+	 }
+	 Todo.findById(id).then((todo)=>{
+	 	if(!todo){
+	 		return res.status(404).send(); 
+	 	}
+
+	 	res.send({todo});
+
+	 }).catch((e)=>{
+	 	res.status(400).send();
+	 });
+
+},(e)=>{
+
+});
+
+
+// server creation
 app.listen(3000,()=>{
 	console.log('started at 3000');
 });
